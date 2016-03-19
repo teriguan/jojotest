c8ca51d2af074f13aff3fdf7a91c3a9e13a7d44d
Teresa Guan <teriguan@outlook.com>
2016-03-18 19:44:38 -0700

    change registration

--- register_form.php
+++ register_form.php
@@ -2,15 +2,51 @@
 	session_start();
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Register Form</title>
-<link href="css/loginmodule.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
 
-<?php
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Jojo's Dog Grooming - Sign In/Register</title>
+        <link rel="stylesheet" href="Style/base.css" media="screen">
+        <link rel="stylesheet" href="Style/printstyle.css" media="print">
+    </head>
+    
+    <body>
+        
+        <div id="signin">
+            <a href="signin.html">sign in/register</a>
+        </div> 
+        <header>
+		    <a href="index.html"><h1><img src="Images/logo.png"
+                 width="200" height="200" alt="Jojo's Dog Grooming-Home"></h1></a>
+		</header>
+        <nav>
+            <div class="dropdown">
+                <ul id="nav">
+                    <li><a href="aboutus.html">About Us</a></li>
+                    <li><a href="testimonials.html">Testimonials</a></li>
+                    <li><a href="services.html" id="drop">Services</a>
+					    <div class="dropdowncontent">
+						    <ul>
+                                <li><a href="Services/pricing.html">Pricing</a></li>
+					            <li><a href="Services/booking.html">Booking</a></li>
+					            <li><a href="Services/faqs.html">FAQs</a></li>
+                            </ul>
+			            </div></li>
+                    <li><a href="contactus.html">Contact Us</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+		<div id="content">
+            <div id="leftcol">
+				<!--Sign in form begins here-->
+				<img src="Images/alaskanEskimo.png" height="250" width="250" alt="alaskan eskimo">
+			</div>
+			<div id="rightcol">
+				<!--Regstration form begins here-->
+				<h2>Register</h2>
+	<?php
 	if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
 		echo '<ul class="err">';
 		foreach($_SESSION['ERRMSG_ARR'] as $msg) {
@@ -19,7 +55,7 @@
 		echo '</ul>';
 		unset($_SESSION['ERRMSG_ARR']);
 	}
-?>
+    ?>
 
 <form id="registerForm" name="registerForm" method="post" action="register.php">
   <table width="300" border="0" align="center" cellpadding="2" cellspacing="0">
@@ -49,5 +85,29 @@
     </tr>
   </table>
 </form>
-</body>
+			</div>
+        </div>
+		<footer id="footer">
+		    <div id="footernav">
+                <ul>
+                    <li><a href="signIn.html">Sign in / Register</a></li>
+                    <li><a href="aboutUs.html">About Us</a></li>
+                    <li><a href="testimonials.html">Testimonials</a></li>
+                    <li><a href="services.html">Services</a>
+                    <li><a href="Services/pricing.html">Pricing</a></li>
+					<li><a href="Services/booking.html">Booking</a></li>
+					<li><a href="Services/faqs.html">FAQs</a></li>
+                    <li><a href="contactus.html">Contact Us</a></li>
+                </ul>
+            </div>
+            <p>Follow us:</p>
+            <p><a href="#"><img src="Images/facebook.png"
+                 width="50" height="50" alt="Facebook"></a>
+                <a href="#"><img src="Images/instagram.png"
+                 width="50" height="50" alt="instagram"></a>
+                <a href="#"><img src="Images/twitter.png"
+                 width="50" height="50" alt="Twitter"></a></p>
+		</footer>
+        <script src="Script/signIn.js"></script>
+    </body>
 </html>
